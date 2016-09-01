8abaeaef4025798ca3a6830a468a28f521aebe95
rawlingsj <rawlingsj80@gmail.com>
2016-09-01 09:03:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# goimprove
+```
+goimprove -r fabric8io/fabric8
+```
+
+goimprove will retrive useful information and metrics and optionally send them to ElasticSearch in order to perform powerful queries and visualisations with Kibana.
+
+Currently it only supports getting the number of downloads a particular github release has had.  The project can be extended to retrieve anything that can be useful and send to ElasticSearch.
+
+It's aim is to be run from the CLI or in Kubernetes pod that will watch for events
+
+Ideally we'd expose a REST API so that a monitoring tool such as Prometheus can coe along and scrape any metrics instead of pushing to ElasticSearch here.
+  
+## Getting started
+
+### Install / Update & run
+
+Get latest download URL from [goimprove releases](https://github.com/fabric8io/goimprove/releases)
+
+```sh
+sudo rm /tmp/goimprove
+sudo rm -rf /usr/bin/goimprove
+mkdir /tmp/goimprove
+curl --retry 999 --retry-max-time 0  -sSL [[ADD DOWNLOAD URL HERE]] | tar xzv -C /tmp/goimprove
+chmod +x /tmp/goimprove/goimprove
+sudo mv /tmp/goimprove/* /usr/bin/
+```
+
+### Usage
+
+```
+goimprove is used to gather stats and metrics of various types and expose via rest to be scraped by a metrics tool
+       								Find more information at http://fabric8.io.
+
+Usage:
+  goimprove [flags]
+  goimprove [command]
+
+Available Commands:
+  gh-downloads retrives the number of downloads of a GitHub project release
+  version      Display version & exit
+
+Flags:
+  -y, --yes   assume yes
+
+Use "goimprove [command] --help" for more information about a command.
+```
+
+## Development
+
+### Prerequisites
+
+Install [go version 1.4](https://golang.org/doc/install)
+
+
+### Building
+
+```sh
+git clone git@github.com:fabric8io/goimprove.git $GOPATH/src/github.com/fabric8io/goimprove
+./make
+```
+
+Make changes to *.go files, rerun `make` and run the generated binary..
+
+e.g.
+
+```sh
+./build/goimprove -r fabric8io/fabric8
+
+```
---
+++ cmds/ghDownloads.go
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cmds
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/fabric8io/goimprove/util"
+	"github.com/google/go-github/github"
+	"github.com/spf13/cobra"
+)
+
+const (
+	// See http://golang.org/pkg/time/#Parse
+	timeFormat = "2006-01-02 15:04 MST"
+)
+
+// NewCmdGitHubDownloads retrives the number of downloads of a GitHub project release
+func NewCmdGitHubDownloads() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "gh-downloads",
+		Short: "retrives the number of downloads of a GitHub project release",
+		Long:  `retrives the number of downloads of a GitHub project release`,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			showBanner()
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			repo := cmd.Flags().Lookup("repository").Value.String()
+			util.Infof("Getting release downloads numbers for GitHub project %s\n", repo)
+
+			org := strings.Split(repo, "/")[0]
+			project := strings.Split(repo, "/")[1]
+
+			client := github.NewClient(nil)
+
+			opt := &github.ListOptions{
+				Page:    0,
+				PerPage: 100,
+			}
+			// get all pages of results
+			var allReleases []*github.RepositoryRelease
+			for {
+				releases, resp, err := client.Repositories.ListReleases(org, project, opt)
+				if err != nil {
+					util.Errorf("Unable to list repositories by org %s %v", "fabric8io", err)
+				}
+				allReleases = append(allReleases, releases...)
+				if resp.NextPage == 0 {
+					break
+				}
+				opt.Page = resp.NextPage
+			}
+			grandTotal := 0
+			var previousReleaseTimeStamp github.Timestamp
+			for v := range allReleases {
+				release := allReleases[v]
+				tag := *release.Name
+				releaseDate := *release.PublishedAt
+				duration := previousReleaseTimeStamp.Time.Sub(releaseDate.Time)
+				totalDownloadCount := 0
+				if tag != "" {
+					for w := range release.Assets {
+						asset := release.Assets[w]
+						totalDownloadCount = totalDownloadCount + *asset.DownloadCount
+					}
+
+					d := duration.Hours() / 24
+					days := strconv.FormatFloat(d, 'f', 6, 64)
+
+					util.Infof("Tag %s published had %v downloads and was available for %s days\n", tag, totalDownloadCount, days)
+
+				}
+				previousReleaseTimeStamp = releaseDate
+				grandTotal = grandTotal + totalDownloadCount
+			}
+			util.Infof("\nGrand total of %v downloads\n", grandTotal)
+		},
+	}
+	cmd.PersistentFlags().StringP("repository", "r", "", "the GitHub repository to get the release download numbers e.g. fabric8io/gofabric8")
+
+	return cmd
+}
